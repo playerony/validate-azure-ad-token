e457b2966d84a13641b18a38c0c2f2e76daecc31
playerony <pawel.wojtasinski.1995@gmail.com>
2021-09-26 21:20:33 +0200

    docs: create-documentation

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,36 @@ import { validateTokenHeader } from './utils/validate-token-header';
 import { validateTokenClaims } from './utils/validate-token-claims';
 import { validateTokenWithGraphApi } from './utils/validate-token-with-graph-api';
 
-import { IValidationOptions } from './index.types';
+/**
+ * @packageDocumentation Function to validate access token received from azure active directory. Useful when you're using a msal library to authenticate users on the frontend and you wanna verify Microsoft tokens in the API.
+ */
 
 /**
- * @packageDocumentation Simple function to validate access token received from azure active directory. Useful when you're using a msal library to authenticate users on the frontend and you wanna verify Microsoft tokens in the API.
+ * The interface represents required fields to process access token validation
+ *
+ * @public
  */
+export interface IValidationOptions {
+  /**
+   * Your Microsoft 365 tenant ID is a globally unique identifier (GUID) that is different than your organization name or domain.
+   */
+  tenantId: string;
+
+  /**
+   * The set of scopes exposed by your application for which the client application has requested (and received) consent.
+   */
+  scopes: string[];
+
+  /**
+   * Identifies the intended recipient of the token - its audience. In v2.0 tokens, this is always the client ID of the API, while in v1.0 tokens it can be the client ID or the resource URI used in the request, depending on how the client requested the token.
+   */
+  audience: string;
+
+  /**
+   * Only present in v1.0 tokens. The application ID of the client using the token. The application can act as itself or on behalf of a user. The application ID typically represents an application object, but it can also represent a service principal object in Azure AD.
+   */
+  applicationId: string;
+}
 
 /**
  * Validate azure active directory access token
@@ -25,18 +50,14 @@ export async function validate(
     throw new Error('The access token could not be decoded');
   }
 
-  try {
-    await validateTokenHeader(decodedAccessToken.header, tenantId, applicationId);
+  await validateTokenHeader(decodedAccessToken.header, tenantId, applicationId);
 
-    validateTokenClaims(decodedAccessToken.payload, {
-      scopes,
-      audience,
-      tenantId,
-      applicationId,
-    });
+  validateTokenClaims(decodedAccessToken.payload, {
+    scopes,
+    audience,
+    tenantId,
+    applicationId,
+  });
 
-    await validateTokenWithGraphApi(accessToken, decodedAccessToken.payload);
-  } catch (error: unknown) {
-    console.log(error);
-  }
+  await validateTokenWithGraphApi(accessToken, decodedAccessToken.payload);
 }
