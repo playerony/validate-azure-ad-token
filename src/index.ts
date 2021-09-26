fb91e36f5f074ec6e7f76a15599e2d7aedc5318f
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2021-09-26 20:57:35 +0400

    chore: validate token claims (#2)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { decodeAccessToken } from './utils/decode-access-token';
 import { validateTokenHeader } from './utils/validate-token-header';
+import { validateTokenClaims } from './utils/validate-token-claims';
+
+import { IValidationOptions } from './index.types';
 
 /**
  * @packageDocumentation Simple function to validate access token received from azure active directory. Useful when you're using a msal library to authenticate users on the frontend and you wanna verify Microsoft tokens in the API.
@@ -12,14 +15,24 @@ import { validateTokenHeader } from './utils/validate-token-header';
  *
  * @public
  */
-export async function validate(accessToken: string): Promise<void> {
+export async function validate(
+  accessToken: string,
+  { scopes, audience, tenantId, applicationId }: IValidationOptions,
+): Promise<void> {
   const decodedAccessToken = decodeAccessToken(accessToken);
   if (!decodedAccessToken) {
     throw new Error('The access token could not be decoded');
   }
 
   try {
-    await validateTokenHeader(decodedAccessToken.header, '123', '123');
+    await validateTokenHeader(decodedAccessToken.header, tenantId, applicationId);
+
+    validateTokenClaims(decodedAccessToken.payload, {
+      scopes,
+      audience,
+      tenantId,
+      applicationId,
+    });
   } catch (error: unknown) {
     console.log(error);
   }
