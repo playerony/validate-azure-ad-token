c5d48a1588361289e97019e7e3faed8627a61ed7
playerony <pawel.wojtasinski.1995@gmail.com>
2023-03-26 11:47:20 +0200

    ci: bump all npm packages

--- src/core/validate-token-with-graph-api.ts
+++ src/core/validate-token-with-graph-api.ts
@@ -12,20 +12,15 @@ const callGraphApi = async (accessToken: string) =>
     headers: { authorization: `Bearer ${accessToken}` },
   });
 
-export const validateTokenWithGraphApi = async (
-  accessToken: string,
-  tokenPayload: JwtPayload,
-): Promise<void> => {
+export const validateTokenWithGraphApi = async (accessToken: string, tokenPayload: JwtPayload): Promise<void> => {
   if (!tokenPayload.oid) {
-    throw new Error(`The token's payload does not contain "oid" property`);
+    throw new Error('The token\'s payload does not contain "oid" property');
   }
 
-  const response = await axiosTryCatchWrapper(
-    () => callGraphApi(accessToken),
-    'Graph API tells your token is invalid',
-  );
+  const response = await axiosTryCatchWrapper(() => callGraphApi(accessToken), 'Graph API tells your token is invalid');
 
   const isSameId = response.data.id === tokenPayload.oid;
+
   if (!isSameId) {
     throw new Error('The token`s payload "oid" value does not match with "id" one from graph API');
   }
